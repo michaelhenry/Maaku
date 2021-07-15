3534ca11276c234c1e2dc71457755b75d315be53
Michael Henry Pantaleon <me@iamkel.net>
2021-07-15 14:59:53 +1000

    removed unit test (temporarily)

--- Package.swift
+++ Package.swift
@@ -17,16 +17,16 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/KristopherGBaker/libcmark_gfm.git", from: "0.29.3"),
-        .package(url: "https://github.com/Quick/Nimble.git", from: "8.0.0"),
-        .package(url: "https://github.com/Quick/Quick.git", from: "2.1.0")
+//        .package(url: "https://github.com/Quick/Nimble.git", from: "8.0.0"),
+//        .package(url: "https://github.com/Quick/Quick.git", from: "2.1.0")
     ],
     targets: [
         .target(
             name: "Maaku",
             dependencies: ["libcmark_gfm"]),
-        .testTarget(
-            name: "MaakuTests",
-            dependencies: ["Maaku", "Nimble", "Quick"]),
+//        .testTarget(
+//            name: "MaakuTests",
+//            dependencies: ["Maaku", "Nimble", "Quick"]),
     ],
     swiftLanguageVersions: [.v5]
 )
